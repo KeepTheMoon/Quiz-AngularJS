9a917ed9e9f3675bb1458103173eb9f517a51545
KeepTheMoon <rouger.baptiste@gmail.com>
2017-05-02 22:13:24 +0200

    Last maj

--- js/factories/dataservice.js
+++ js/factories/dataservice.js
@@ -13,67 +13,19 @@
 
         return dataObj;
     }
-    var correctAnswers = [0, 2, 3];
-    var quizQuestions  = [
-        {
-            type: "text",
-            text: "How much can a loggerhead weigh?",
-            possibilities: [
-                {
-                    answer: "Up to 20kg"
-                },
-                {
-                    answer: "Up to 115kg"
-                },
-                {
-                    answer: "Up to 220kg"
-                },
-                {
-                    answer: "Up to 500kg"
-                }
-            ],
-            selected: null,
-            correct: null
-        },
-        {
-            type: "text",
-            text: "What is the typical lifespan of a Green Sea Turtle?",
-            possibilities: [
-                {
-                    answer: "150 years"
-                },
-                {
-                    answer: "10 years"
-                },
-                {
-                    answer: "80 years"
-                },
-                {
-                    answer: "40 years"
-                }
-            ],
-            selected: null,
-            correct: null
-        },
-        {
-            type: "text",
-            text: "How Heavy can a leatherback turtle be?",
-            possibilities: [
-                {
-                    answer: "900kg"
-                },
-                {
-                    answer: "40kg"
-                },
-                {
-                    answer: "110kg"
-                },
-                {
-                    answer: "300kg"
-                }
-            ],
-            selected: null,
-            correct: null
-        }
-    ];
+
+    var correctAnswers = function($resource){
+	return $resource('URL', { },
+		{
+			query: {method:'GET', isArray:true, headers: { 'theme': '?' }},
+		});
+};
+
+    var quizQuestions = function($resource){
+	return $resource('URL', { },
+		{
+			query: {method:'GET', isArray:true, headers: { 'theme': '?' }},
+		});
+};
+
 })();
